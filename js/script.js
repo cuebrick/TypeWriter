99a6997956f3829303b647ca91e50a5e97d53398
Jong-sun Won <cuebrick@gmail.com>
2017-08-31 16:32:33 +0900

    시작 버튼 추가, 시간 측정 타이머 추가

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  **********************************************************/
 $(function () {
 	// POWERMODE.colorful = true ;
-	document.body.addEventListener('input', POWERMODE);
+	document.body.addEventListener('keyup', POWERMODE);
 	_type.initListener();
 });
 
@@ -11,25 +11,46 @@ var _type = {
 	_props_ : {
 
 	},
-
+	timestamps: [],
+	timer: 0,
 	data: [],
 	keyData:[],
 
 	initListener: function () {
 		$(window).keyup(function (e) {
 			var code = e.keyCode;
-			console.log('keydown :', code);
+			console.log('keyup :', code);
 			var exampleText = $('#exampleText').text().trim();
 			var inputText = $('#inputText').val().trim();
-			console.log(Hangul.disassemble(exampleText));
-			console.log(Hangul.disassemble(inputText));
+			console.log(Hangul.disassemble(exampleText, true));
+			console.log(Hangul.disassemble(inputText, true));
 			switch (code){
 				case 13:// enter key
-
+					_type.pushTime();
+					clearInterval(_type.timer);
 					break;
 				default:
-
 			}
 		});
+		$('#inputText').focus();
+		$('#startButton').click(function () {
+			_type.pushTime();
+			$('#inputText').focus();
+			var count = 0;
+			_type.timer = setInterval(function () {
+				$('#timeElapse').text((count * 0.1).toFixed(1));
+				++count;
+			}, 100);
+		});
+	},
+
+	pushTime : function () {
+		_type.timestamps.push(new Date().getTime());
+	},
+	getTime : function (index) {
+		if(index === undefined)
+			index = 0;
+
+		return _type.timestamps[index];
 	}
 };
