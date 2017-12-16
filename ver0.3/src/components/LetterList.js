2b8f00aa8c9a3e5c9a4c8054248dfe6eaf79f65d
Jong-sun Won <cuebrick@gmail.com>
2017-12-17 08:38:11 +0900

    LetterList 추가

---
+++ ver0.3/src/components/LetterList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LetterList extends React.Component{
+	render(){
+		return(
+			<div id="letterList" className="letter-list">
+
+			</div>
+		)
+	}
+}
+
+export default LetterList;
--- ver0.3/src/components/SentenceArea.js
+++ ver0.3/src/components/SentenceArea.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import KeyboardLayout from './KeyboardLayout';
+import LetterList from './LetterList';
 
 class SentenceArea extends React.Component{
 	render(){
 		return(
 			<div className="sentence-area">
 				<h3 id="levelTitle">문장연습</h3>
-				<div id="letterList" className="letter-list">
 
-				</div>
+				<LetterList/>
+
 				<div className="button-ui">
 					<button id="goLevelListButton">목록으로</button>
 				</div>
