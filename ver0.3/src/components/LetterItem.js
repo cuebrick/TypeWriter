b301589f22508054a6af5429d1ca3d83e38b3350
Jong-sun Won <cuebrick@gmail.com>
2017-12-29 09:13:41 +0900

    문자 입력을 ref 참조 방식으로 구현

--- ver0.3/src/components/LetterItem.js
+++ ver0.3/src/components/LetterItem.js
@@ -4,14 +4,23 @@ class LetterItem extends React.Component{
 
 	constructor(props){
 		super(props);
+		this.state = {input: ''}
 	}
+	input(char){
+		console.log('LetterItem.input(): ', char);
+		this.setState({input: char})
+	}
+
 	render(){
 		let enterKeyClassName = (this.props.char === '↩') ? ' enter-key' : '';
+
+		let typing = (this.props.typing) ? this.props.typing : '';
 
 		return(
 			<div className={"letter" + enterKeyClassName}>
 				<div className="text">{this.props.char}</div>
-				<div className="typing"></div>
+				<div className="typing">{this.state.input}</div>
+				{/*<div className="typing">{typing}</div>*/}
 			</div>
 		)
 	}
--- ver0.3/src/components/LetterList.js
+++ ver0.3/src/components/LetterList.js
@@ -1,37 +1,234 @@
 import React from 'react';
 import LetterItem from './LetterItem';
+import Keymap from '../json/keymap';
+import Level from './Level';
+import Hangul from '../lib/hangul';
 
 class LetterList extends React.Component{
+
 
 	constructor(props){
 		super(props);
 
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+
 		this.state = {
-			isShow: true
+			typing: []
 		}
 	}
 
-	show(){
-		this.setState({isShow: true});
+	componentDidMount(){
+		this.addKeyboardEvent();
 	}
 
-	hide(){
-		this.setState({isShow: false});
+	componentWillUnmount(){
+		this.removeKeyboardEvent();
+	}
+
+	addKeyboardEvent(){
+		window.addEventListener('keydown', this.handleKeyDown);
+	}
+
+	removeKeyboardEvent(){
+		window.removeEventListener('keydown', this.handleKeyDown);
+	}
+
+	handleKeyDown(e){
+		// console.log('LetterList.keydown: ', this, e.keyCode);
+		let code = e.keyCode;
+		switch (code){
+			// 아무것도 하지 않는 케이스 + 기본 키 입력도 막아야 하는 케이스
+			case 9: // tab
+			case 18: // alt - left
+			case 21: // alt - right
+				e.preventDefault();
+				break;
+
+			case 16: // shift
+				// _play.keyboard.shifting();
+				break;
+
+			case 17: // ctrl
+			case 93: // context menu
+			case 25: // ctrl - right
+			case 91: // win key
+			case 20: // caps
+			case 112: // F1
+			case 113: // F2
+			case 114: // F3
+			case 115: // F4
+			case 116: // F5
+			case 117: // F6
+			case 118: // F7
+			case 119: // F8
+			case 120: // F9
+			case 121: // F10
+			case 122: // F11
+			case 123: // F12
+			case 37: // left
+			case 38: // up
+			case 39: // right
+			case 40: // down
+				break;
+
+			case 8: // backspace
+				// recordKey(e);
+				//
+				// if(_play.level.keyBuffer.length){
+				// 	_play.level.keyBuffer.pop();
+				// 	evaluate();
+				// }else{
+				// 	removeEvaluate();
+				// 	setPrevIndex();
+				// }
+				//
+				// updateDisplay(_play.level.keyBuffer);
+				// evaluate();
+				break;
+
+			// 한글, 영문 입력 a~z, A~Z, ㄱ~ㅎ, ㅏ~ㅣ
+			case 65:
+			case 66:
+			case 67:
+			case 68:
+			case 69:
+			case 70:
+			case 71:
+			case 72:
+			case 73:
+			case 74:
+			case 75:
+			case 76:
+			case 77:
+			case 78:
+			case 79:
+			case 80:
+			case 81:
+			case 82:
+			case 83:
+			case 84:
+			case 85:
+			case 86:
+			case 87:
+			case 88:
+			case 89:
+			case 90:
+				this.inputLetter(e);
+				break;
+
+			// 엔터키의 경우 개행이 있는 문장에서는 개행하고, 없는 문장에서는 레벨을 끝내려는 유저의 의도로 판단함
+			// 개행문장의 경우 개행이 아직 아닌데 엔터키를 누르면 개행전까지의 타이핑을 전부 오타처리하고 개행부터 다시 판단.
+			case 13: // enter
+				e.preventDefault();
+				this.inputLetter(e);
+				// enterKeyLoop();
+				break;
+
+			// 숫자, 기호 입력 케이스(숫자, 기호 입력은 입력 즉시 다음 칸으로 움직이고 한글 도깨비불 현상이 없음)
+			case 32: // space bar
+			case 48: // 0
+			case 49: // 1
+			case 50: // 2
+			case 51: // 3
+			case 52: // 4
+			case 53: // 5
+			case 54: // 6
+			case 55: // 7
+			case 56: // 8
+			case 57: // 9
+			case 186: // ;
+			case 187: // =
+			case 188: // ,
+			case 189: // -
+			case 190: // .
+			case 191: // slash
+			case 192: // `
+			case 219: // [
+			case 220: // backslash
+			case 221: // ]
+			case 222: // '
+				e.preventDefault();
+				this.inputLetter(e);
+				this.setNextIndex();
+				this.clearBuffer();
+				break;
+
+			default:
+		}
+	}
+
+	inputLetter(e){
+		let level = this.props.level;
+		let code = e.keyCode;
+
+		// this.recordKey(e);
+		let lang = level.language;
+		let shiftKey = (e.shiftKey) ? 's' : 'n';
+		let char = Keymap['code' + code][lang + shiftKey];
+
+		if(level.type === Level.CHARACTER_TYPE){
+			this.updateDisplay([char]);
+			this.setNextIndex();
+			this.clearBuffer();
+		}else{
+			this.addBuffer(char);
+			let letter = Hangul.a(level.buffer);
+			if(letter.length > 1){
+				let letters = Hangul.d(letter, true);
+				this.updateDisplay(letters.shift());
+				// this.evaluate();
+				this.setNextIndex();
+				level.buffer = letters[0];
+			}
+			this.updateDisplay(level.buffer);
+			// this.evaluate();
+		}
+	}
+
+	clearBuffer(){
+		this.props.level.buffer = [];
+	}
+	addBuffer(char){
+		this.props.level.buffer.push(char);
+	}
+
+	setNextIndex(){
+		// this.setState({index: this.state.index++});
+		this.props.level.index++;
+	}
+
+	updateDisplay(charArray){
+		// this.setState({typing: Hangul.a(charArray)});
+		let item = this.refs['letterItem'+this.props.level.index];
+		item.input(Hangul.a(charArray));
 	}
 
 	render(){
-		// console.log('LetterList.render:>>', (this.props.level) ? this.props.level.text : '');
+		// console.log('LetterList.render:>>');
 		let letters = [];
 		if(this.props.level){
 			letters = this.props.level.text.split('');
 		}
+
+		let letterItems = letters.map((letter, i) => {
+			return(
+				<LetterItem ref={'letterItem'+i} char={letter} key={i}/>
+			)
+
+			/*if(i === this.props.level.index){
+				return(
+					<LetterItem ref={'letterItem'+i} char={letter} key={i} typing={this.state.typing}/>
+				)
+			}else{
+				return(
+					<LetterItem ref={'letterItem'+i} char={letter} key={i}/>
+				);
+			}*/
+		});
+
 		return(
-			<div id="letterList" className={"letter-list "  + (this.state.isShow ? 'show' : 'hide')}>
-				{letters.map((letter, i) => {
-					return(
-						<LetterItem char={letter} key={i}/>
-					);
-				})}
+			<div id="letterList" className="letter-list">
+				{letterItems}
 			</div>
 		)
 	}
