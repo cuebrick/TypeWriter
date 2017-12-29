636672c1783e1aad3c0e62b24efda0fce69be8c3
Jong-sun Won <cuebrick@gmail.com>
2017-12-29 09:11:29 +0900

    Level 을 React.component 를 상속받지 않고 구현 변경

--- ver0.3/src/components/Level.js
+++ ver0.3/src/components/Level.js
@@ -1,19 +1,50 @@
-import React from 'react';
+class Level{
 
-class Level extends React.Component{
+	static get CHARACTER_TYPE(){
+		return 'character';
+	}
+	static get SENTENCE_TYPE(){
+		return 'sentence';
+	}
+
+	id;
+	grade;
+	type;
+	language;
+	title;
+	text;
+
+	index;
+	intervalId;
+	buffer;
+
 	constructor(props){
-		super(props);
+		// super(props);
 
-		this.state = {
+		this.id = props.id;
+		this.grade = props.grade;
+		this.type = props.type;
+		this.language = props.language;
+		this.title = props.title;
+		this.text = props.text;
+
+		this.index = 0;
+		this.intervalId = undefined;
+		this.buffer = [];
+
+		/*this.state = {
 			id: this.props.id,
 			grade: this.props.grade,
 			type: this.props.type,
 			language: this.props.language,
 			title: this.props.title,
-			text: this.props.text
-		}
+			text: this.props.text,
+
+			intervalId: 0
+		}*/
 	}
-	get id(){
+
+	/*get id(){
 		return this.state.id
 	}
 	get grade(){
@@ -31,6 +62,15 @@ class Level extends React.Component{
 	get text(){
 		return this.state.text
 	}
+
+
+	get intervalId(){
+		return this.state.intervalId;
+	}
+	set intervalId(id){
+		console.log(id);
+		this.setState({intervalId: id});
+	}*/
 }
 
 export default Level;
