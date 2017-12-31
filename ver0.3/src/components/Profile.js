5ded9e611a44a270dcae82efd8d2ab0f63f24d13
Jong-sun Won <cuebrick@gmail.com>
2018-01-01 08:28:33 +0900

    Add user profile layer and show/hide toggle.

--- ver0.3/src/components/Profile.js
+++ ver0.3/src/components/Profile.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 class Profile extends React.Component{
+	constructor(props){
+		super(props);
+		this.toggleUserLayer = this.toggleUserLayer.bind(this);
+		this.state = {
+			isShowUserLayer : false
+		}
+	}
+
+	toggleUserLayer(){
+		this.setState({"isShowUserLayer": !this.state.isShowUserLayer});
+	}
+
 	render(){
+		let layerShowClassName = (this.state.isShowUserLayer) ? ' show' : ' hide';
 		return(
-			<div className="profile">
+			<div className="profile" onClick={this.toggleUserLayer}>
 				<div className="profile-image">
 					<img src={"/images/icon/profile-icon-1.svg"}/>
 				</div>
 				<div className="user-info">
 					<div className="user-grade">수련생</div>
 					<div className="user-name">세계의끝</div>
+				</div>
+				<div className={"user-layer" + layerShowClassName}>
+					<button>새로운 사용자</button>
 				</div>
 			</div>
 		)
