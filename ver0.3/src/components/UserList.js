6b3c6a155b9a660faaee806b8332b50f01428c19
Jong-sun Won <cuebrick@gmail.com>
2018-01-04 13:20:50 +0900

    Remove mouse interaction - conditional render()

--- ver0.3/src/components/UserList.js
+++ ver0.3/src/components/UserList.js
@@ -14,19 +14,29 @@ class UserList extends React.Component{
 				{
 					userList.map((data) => {
 						let isCurrentUser = (this.props.currentUserId === data.id);
-						return(
-							<li key={data.id}>
-								{
-									isCurrentUser &&
+						if(isCurrentUser){
+							return(
+								<li key={data.id} className="current-user">
 									<img src={"/images/checked.svg"} className="checked" width="15" />
-								}
-								<div className="user-image">
-									<img src={"/images/icon/profile-icon-"+ data.icon +".svg"}/>
-								</div>
-								<div className="user-grade">{data.grade}</div>
-								<div className="user-name">{data.name}</div>
-							</li>
-						)
+									<div className="user-image">
+										<img src={"/images/icon/profile-icon-"+ data.icon +".svg"}/>
+									</div>
+									<div className="user-grade">{data.grade}</div>
+									<div className="user-name">{data.name}</div>
+								</li>
+							)
+						} else {
+							return(
+								<li key={data.id}>
+									<div className="user-image">
+										<img src={"/images/icon/profile-icon-"+ data.icon +".svg"}/>
+									</div>
+									<div className="user-grade">{data.grade}</div>
+									<div className="user-name">{data.name}</div>
+								</li>
+							)
+						}
+
 					})
 				}
 				{/*<li>
