af42406609c2d02eadb5e3c519bc3feb6334f0e0
Jong-sun Won <cuebrick@gmail.com>
2018-02-04 09:34:17 +0900

    ReportView CSS styling

--- ver0.3/src/components/ReportView.js
+++ ver0.3/src/components/ReportView.js
@@ -2,25 +2,42 @@ import React from 'react';
 
 class ReportView extends React.Component{
 	render(){
+		let stars = [...Array(3)].map((v, i) => {
+			if(i < Number(this.props.level.result.star)){
+				return <span key={"key"+i} className="star" />
+			} else {
+				return <span key={"key"+i} className="star incomplete" />
+			}
+		});
+
+		let result = this.props.level.result;
+		console.log('=============' +
+			result.totalLetterLen);
 		return(
 			<div className="report-view">
 				<div className="report-view-inner">
-					<h4>{this.props.level.title}</h4>
+					<h3>{this.props.level.title}</h3>
 					<div className="stars">
-						{
-							[...Array(3)].map((v, i) => {
-								if(i < Number(this.props.level.result.star)){
-									return <span key={"key"+i} className="star" />
-								} else {
-									return <span key={"key"+i} className="star incomplete" />
-								}
-							})
-						}
+						{stars}
 					</div>
 					<div className="description">
 						{
-							JSON.stringify(this.props.level.result)
+							<dl>
+								<dt>전체 글자 수</dt>
+								<dd>{result.missLetterCount + '/' + result.totalLetterLen}</dd>
+								<dt>전체 타이핑 수</dt>
+								<dd>{result.missCount + '/' + result.totalCharLen}</dd>
+								<dt>시간</dt>
+								<dd>{result.elapseTime + '초'}</dd>
+								<dt>분당 타수</dt>
+								<dd>{result.speedPerMin + '타'}</dd>
+								<dt>정확도</dt>
+								<dd>{result.accuracy + '%'}</dd>
+							</dl>
 						}
+					</div>
+					<div className="report-view-msg">
+						esc 키를 누르면 목록으로, enter 키를 누르면 다음 단계로 넘어갑니다.
 					</div>
 				</div>
 			</div>
