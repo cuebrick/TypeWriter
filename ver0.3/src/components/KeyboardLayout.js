4f4a6483de1ee4e5a34a0285461da973372a6f70
Jong-sun Won <cuebrick@gmail.com>
2018-02-18 08:06:21 +0900

    Reposition English letter on keycap

--- ver0.3/src/components/KeyboardLayout.js
+++ ver0.3/src/components/KeyboardLayout.js
@@ -54,6 +54,8 @@ class KeyboardLayout extends React.Component{
 	}
 
 	render(){
+		// 영문 키캡의 경우 기호들과는 다르게 쉬프트 된 문자(대문자) 가 표시되어 있음.
+		// Keycap 에 주어진 props 들은 실제 프로그래밍적으로 의미가 있는 것은 아니고 화면 표시용으로만 사용.
 		return(
 			<div className="keyboard">
 				<div className="row">
@@ -74,44 +76,44 @@ class KeyboardLayout extends React.Component{
 				</div>
 				<div className="row">
 					<Keycap keyCode={8} enn={"tab"} keyType={"type2 disable"}/>
-					<Keycap keyCode={81} enn={"Q"} krs={"ㅃ"} krn={"ㅂ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={87} enn={"W"} krs={"ㅉ"} krn={"ㅈ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={69} enn={"E"} krs={"ㄸ"} krn={"ㄷ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={82} enn={"R"} krs={"ㄲ"} krn={"ㄱ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={84} enn={"T"} krn={"ㅅ"} krs={"ㅆ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={89} enn={"Y"} krn={"ㅛ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={85} enn={"U"} krn={"ㅕ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={73} enn={"I"} krn={"ㅑ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={79} enn={"O"} krn={"ㅐ"} krs={"ㅒ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={80} enn={"P"} krn={"ㅔ"} krs={"ㅖ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={81} ens={"Q"} krs={"ㅃ"} krn={"ㅂ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={87} ens={"W"} krs={"ㅉ"} krn={"ㅈ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={69} ens={"E"} krs={"ㄸ"} krn={"ㄷ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={82} ens={"R"} krs={"ㄲ"} krn={"ㄱ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={84} ens={"T"} krn={"ㅅ"} krs={"ㅆ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={89} ens={"Y"} krn={"ㅛ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={85} ens={"U"} krn={"ㅕ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={73} ens={"I"} krn={"ㅑ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={79} ens={"O"} krn={"ㅐ"} krs={"ㅒ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={80} ens={"P"} krn={"ㅔ"} krs={"ㅖ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={219} enn={"["} ens={""} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={221} enn={"]"} ens={""} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={220} enn={"\\"} ens={"|"} krn={"\\"} keyType={"type2"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 				</div>
 				<div className="row">
 					<Keycap keyCode={20} enn={"caps"} keyType={"type3 disable"}/>
-					<Keycap keyCode={65} enn={"A"} krn={"ㅁ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={83} enn={"S"} krn={"ㄴ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={68} enn={"D"} krn={"ㅇ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={70} enn={"F"} krn={"ㄹ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={71} enn={"G"} krn={"ㅎ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={72} enn={"H"} krn={"ㅗ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={74} enn={"J"} krn={"ㅓ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={75} enn={"K"} krn={"ㅏ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={76} enn={"L"} krn={"ㅣ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={65} ens={"A"} krn={"ㅁ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={83} ens={"S"} krn={"ㄴ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={68} ens={"D"} krn={"ㅇ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={70} ens={"F"} krn={"ㄹ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={71} ens={"G"} krn={"ㅎ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={72} ens={"H"} krn={"ㅗ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={74} ens={"J"} krn={"ㅓ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={75} ens={"K"} krn={"ㅏ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={76} ens={"L"} krn={"ㅣ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={186} enn={";"} ens={":"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={222} enn={"'"} ens={"\""} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={13} enn={"enter"} keyType={"type5"} nextCode={this.props.nextCode}/>
 				</div>
 				<div className="row">
 					<Keycap keyCode={16} enn={"shift"} keyType={"type5"} nextCode={this.props.nextCode}/>
-					<Keycap keyCode={90} enn={"Z"} krn={"ㅋ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={88} enn={"X"} krn={"ㅌ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={67} enn={"C"} krn={"ㅊ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={86} enn={"V"} krn={"ㅍ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={66} enn={"B"} krn={"ㅠ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={78} enn={"N"} krn={"ㅜ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
-					<Keycap keyCode={77} enn={"M"} krn={"ㅡ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={90} ens={"Z"} krn={"ㅋ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={88} ens={"X"} krn={"ㅌ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={67} ens={"C"} krn={"ㅊ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={86} ens={"V"} krn={"ㅍ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={66} ens={"B"} krn={"ㅠ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={78} ens={"N"} krn={"ㅜ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
+					<Keycap keyCode={77} ens={"M"} krn={"ㅡ"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={188} enn={","} ens={"<"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={190} enn={"."} ens={">"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
 					<Keycap keyCode={191} enn={"/"} ens={"?"} nextCode={this.props.nextCode} isShifting={this.state.isShifting}/>
