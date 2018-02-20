a3558664325b62bbfc9ded32fc99a5acfb36aeab
Jong-sun Won <cuebrick@gmail.com>
2018-02-20 09:40:47 +0900

    Typo.

--- ver0.3/src/pages/Main.js
+++ ver0.3/src/pages/Main.js
@@ -51,7 +51,7 @@ class Main extends React.Component{
 					<Link to="/levels"><button>단계 목록으로 이동</button></Link>
 					{
 						this.state.isShowNext &&
-						<Link to={"/levels/" + this.state.nextLevel.id}><button>
+						<Link to={"/typing/" + this.state.nextLevel.id}><button>
 							{this.state.nextLevel.title}
 							<small>{" (" + this.state.nextLevel.subtitle + ") "}</small>
 							이어서 하기
