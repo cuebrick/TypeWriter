a08994e5f0dd8ce2c7b106882d38d3292dfa3abf
Jong-sun Won <cuebrick@gmail.com>
2018-04-18 13:11:49 +0900

    Add contents in About.js page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,9 +5,18 @@ class About extends React.Component{
 	render(){
 		return(
 			<div className="container">
-				<h3>Typing Play 에 오신것을 환영합니다.</h3>
+				<h3>Typing Play 안내</h3>
 				<div className="main-text font-LexiSaebomR">
 					<Information/>
+				</div>
+
+				<h3>사용자 등록</h3>
+				<div className="main-text font-LexiSaebomR">
+					서버를 통하는 회원 가입 절차는 없습니다.<br/>
+					모든 기록은 여러분의 pc에 로컬로 저장되고 다른 pc로 공유 되지 않습니다. <br/>
+					비밀번호나 주민등록번호가 필요하지 않는 대신 다른 pc에서 여러분의 기록을 이어서 플레이 할 수는 없습니다. <br/>
+					다른 pc에서는 새로운 사용자를 만들어 주셔야 합니다.<br/>
+					그러나 하나의 pc 에서 여러 사용자의 기록을 관리 할 수 있도록 사용자를 추가할 수 있습니다. 오른쪽 위의 사용자 아이콘을 선택해 주세요
 				</div>
 			</div>
 		)
