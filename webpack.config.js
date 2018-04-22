4d948f27c147f99664b81922338599c07ff2e72d
Jong-sun Won <cuebrick@gmail.com>
2018-04-23 07:23:23 +0900

    Change build path. Remove public dir.

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ module.exports = {
 	entry: './src/index.js',
 
 	output: {
-		path: __dirname + '/public/',
+		path: __dirname + '/build/',
 		filename: 'bundle.js'
 	},
 
 	devServer: {
 		inline: true,
 		port: 7777,
-		contentBase: __dirname + '/public/',
+		contentBase: __dirname + '/build/',
 		historyApiFallback: true
 	},
 
