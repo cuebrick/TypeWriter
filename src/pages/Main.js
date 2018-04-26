ec0a26fbdebb2a0bbc8067ab8e9dfe4687d675eb
Jong-sun Won <cuebrick@gmail.com>
2018-04-26 15:56:18 +0900

    Remove image on Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -37,9 +37,6 @@ class Main extends React.Component{
 			<div className="container">
 				<h3>Typing Play 에 오신것을 환영합니다.</h3>
 				<div className="main-text font-LexiSaebomR">
-					<div className="main-img">
-						<img src="/images/main-desk.svg" width="90%"/>
-					</div>
 					{
 						this.state.isShowNext === false ?
 						<Information/>
@@ -48,7 +45,6 @@ class Main extends React.Component{
 							다시 만나 반갑습니다. 아래 메뉴 중 하나를 선택할 수 있습니다.
 						</div>
 					}
-
 				</div>
 				<div className="button-ui">
 					<Link to="/levels"><button>단계 목록으로 이동</button></Link>
