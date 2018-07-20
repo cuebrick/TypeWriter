caaab233a4d495424c669366f46d6036a494c3bd
Jong-sun Won <cuebrick@gmail.com>
2018-07-20 13:11:19 +0900

    CSS styling in Error page.

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class NotFound extends React.Component{
 	render(){
 		return(
 			<div className="container">
-				NotFound
+				<div className="error-msg-box">
+					<h3>요청하신 페이지를 찾을 수 없습니다.</h3>
+					<p className="error-text">
+						이 주소는 없어졌거나 잘못된 주소인것 같습니다.<br />
+						주소를 다시 확인해 주세요.
+					</p>
+					<div className="button-ui">
+						<Link to="/">
+							<button className="home-btn"><img src="/images/home.svg" width="15" height="15"/> 첫 화면으로 이동</button>
+						</Link>
+						<Link to="/levels">
+							<button>단계 목록으로 이동</button>
+						</Link>
+					</div>
+				</div>
 			</div>
 		)
 	}
